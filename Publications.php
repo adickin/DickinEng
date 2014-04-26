31b7f10bfff93f338fd5d2d255a30e3713486478
adickin <adamdickin@gmail.com>
2014-04-26 14:30:01 -0600

    publications now link to papers

--- Publications.php
+++ Publications.php
@@ -11,17 +11,17 @@
       </div>
 
       <div class="col-lg-8 text-left" style="float: none; margin: 0 auto;">
-         <p>“Design Challenges Encountered When Expanding a World Class Petrochemical Facility”, J.<br />
+         <p><a href="files/Expanding_Petrochemical_Facility_2001_IEEE_transaction.pdf">“Design Challenges Encountered When Expanding a World Class Petrochemical Facility”, J.<br />
          Dickin, Dr. R. Hannah, S. Randall, and C. Dedeurwaerder.  IEEE Transactions on Industrial<br />
-         Applications, Volume: 37 Issue: 4, July-Aug, 2001.</p>
+         Applications, Volume: 37 Issue: 4, July-Aug, 2001.</a></p>
          <br />
-         <p>“High-Resistance Grounding  –  Avoiding Unnecessary Pitfalls”, D. Murray, J. Dickin, Dr. R.<br />
+         <p><a href="files/High_Resistance_Grounding_2009_IEEE_Transaction.pdf">“High-Resistance Grounding  –  Avoiding Unnecessary Pitfalls”, D. Murray, J. Dickin, Dr. R.<br />
          Hannah, and T. Morin.  IEEE Transactions on Industrial Applications, Volume: 45 Issue: 3, May-<br />
-         June, 2009.</p>
+         June, 2009.</a></p>
          <br />
-         <p>Murray, D., Hanna, R.A. Dickin, J., & Morin, T. "High Resistance Grounding - Avoiding <br />
+         <p><a href="files/Electricity_Today_magazine-April_2008.pdf">Murray, D., Hanna, R.A. Dickin, J., & Morin, T. "High Resistance Grounding - Avoiding <br />
          Misapplication of TVSS and UPS System." Electricity Today Magazine, April 2008, Volume 20,<br />
-         No. 3, page 12.</p>
+         No. 3, page 12.</a></p>
       </div>
    </div>
 </div>
